f176d68ed7edeaea6ad2172eec80f4373c01cf81
Alex Ku <liko0165@gmail.com>
2022-03-01 14:29:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -48,42 +48,4 @@ def error():
     return render_template('error.html',errorMessage=errorMessage)
 
 
-
-
-    
-    
-
-
-    
-        
-
-
-
-
-
-
-
-# @app.route('/data')
-# def handleData():
-#     return 'My Data'
-
-# @app.route('/user/<username>')
-# def handleUser(username):
-#     if username=="AK":
-#         return 'SUP! ' + username
-#     else:
-#         return 'Hello ' + username
-
-# @app.route('/hello')
-# def hello():
-#     name=request.args.get('name','')
-#     session['username'] = name
-#     return '你好 '+ name
-
-# @app.route('/talk')
-# def talk():
-#     name=session['username']
-#     return name + ' 很高興見到你'
-
-
 app.run(port=3000)
